51475c06cbc16864671ef7beba466813c8e19775
Becky139 <rebecca.marriott@outlook.com>
2022-08-29 14:43:01 +0000

    tidy up and add title and favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -45,7 +45,7 @@ function runGame(gameType) {
     } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
     } else if (gameType === "division") {
-        displayDivideQuestion(num1, num2);
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -91,7 +91,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operand2 === "÷") {
+    } else if (operator === "/") {
         return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -144,10 +144,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion(operand1, operand2) {
-
-    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operand2').textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById('operator').textContent = "÷";
-
-}
+function displayDivisionQuestion(operand1, operand2) {
+    operand1 = operand1 * operand2;
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
+    
+} 
